d6477b6918629ad8e5539a38d490b9562bf63da2
Roodjino Cherilus <cherilusjcr@gmail.com>
2023-01-31 10:03:21 -0500

    add todolist code

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './style.css';
+
+import fillList from './modules/fill-render.js';
+import { addTaskEvent, taskDescription, TaskList } from './modules/add-task.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  fillList();
+});
+
+const addBtn = document.getElementById('add-btn');
+
+TaskList.innerHTML += fillList();
+
+// Event Listener for button and Enter Keypress that call addTaskEvent
+addBtn.addEventListener('click', addTaskEvent);
+taskDescription.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    addTaskEvent();
+  }
+});
+
+const clearAll = () => {
+  localStorage.setItem('tasks', JSON.stringify([]));
+  fillList();
+};
+
+const clearAllButton = document.querySelector('.clear');
+clearAllButton.addEventListener('click', clearAll);
